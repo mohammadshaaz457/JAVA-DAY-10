f1272a59b24350eef1cb307ee12985630a12063c
N mohammad shaaz <mohammadshaaz457@gmail.com>
2024-12-11 10:09:38 +0530

    Add files via upload

---
+++ shaaz.java
@@ -0,0 +1,14 @@
+class shaaz//class
+{
+    int a=5;//object
+
+    
+    public static void main(String args[])
+    {
+       shaaz ar=new shaaz();
+       System.out.println(ar.a);
+       
+
+
+    }
+}
