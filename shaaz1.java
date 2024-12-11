31aabd6b5892573a3613efeabb9bd5ff61eaacdb
N mohammad shaaz <mohammadshaaz457@gmail.com>
2024-12-11 10:22:33 +0530

    Add files via upload

---
+++ shaaz1.java
@@ -0,0 +1,13 @@
+//without using the objects 
+import java.util.*;
+class shaaz1
+{
+    public static void s()
+    {
+        System.out.println("HELLO STUDENTS..!");
+
+    }
+    public static void main(String args[]){
+    s();
+    }
+}
