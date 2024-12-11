2cfc114101057161f6b030e3e02abac27d581cb2
N mohammad shaaz <mohammadshaaz457@gmail.com>
2024-12-11 11:12:40 +0530

    Add files via upload

---
+++ key.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class key
+{
+    static void a()
+    {
+        String aString= "shaaz";
+        {
+        System.out.println("HEY!HOW ARE U :"+aString);
+        }
+   }
+    static void a(int b)
+    {
+        System.out.println("HEY!HOW ARE U :"+b);
+    }
+    static void a(int b,int c, String h)
+    {
+        System.out.println("HEY!HOW ARE U :"+b+" "+c);
+    }
+    public static void main(String args[])
+    {
+        a();
+        a(6);
+        a(4,6,"ashraf");
+    }
+
+}
